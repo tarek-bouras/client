6beb1cd3267eec03f147ab7326ed964eef629056
tarek-bouras <82026951+tarek-bouras@users.noreply.github.com>
2023-11-04 16:30:31 +0100

    formating doc

--- src/components/user/GoogleOneTapLogin.jsx
+++ src/components/user/GoogleOneTapLogin.jsx
@@ -1,8 +1,8 @@
-import { Google } from '@mui/icons-material';
-import { Button } from '@mui/material';
-import React, { useState } from 'react';
-import { useValue } from '../../context/ContextProvider';
-import jwtDecode from 'jwt-decode';
+import { Google } from "@mui/icons-material";
+import { Button } from "@mui/material";
+import React, { useState } from "react";
+import { useValue } from "../../context/ContextProvider";
+import jwtDecode from "jwt-decode";
 
 const GoogleOneTapLogin = () => {
   const { dispatch } = useValue();
@@ -13,10 +13,10 @@ const GoogleOneTapLogin = () => {
     const decodedToken = jwtDecode(token);
     const { sub: id, email, name, picture: photoURL } = decodedToken;
     dispatch({
-      type: 'UPDATE_USER',
+      type: "UPDATE_USER",
       payload: { id, email, name, photoURL, token, google: true },
     });
-    dispatch({ type: 'CLOSE_LOGIN' });
+    dispatch({ type: "CLOSE_LOGIN" });
   };
   const handleGoogleLogin = () => {
     setDisabled(true);
@@ -27,7 +27,7 @@ const GoogleOneTapLogin = () => {
       });
       window.google.accounts.id.prompt((notification) => {
         if (notification.isNotDisplayed()) {
-          throw new Error('Try to clear the cookies or try again later!');
+          throw new Error("Try to clear the cookies or try again later!");
         }
         if (
           notification.isSkippedMoment() ||
@@ -38,8 +38,8 @@ const GoogleOneTapLogin = () => {
       });
     } catch (error) {
       dispatch({
-        type: 'UPDATE_ALERT',
-        payload: { open: true, severity: 'error', message: error.message },
+        type: "UPDATE_ALERT",
+        payload: { open: true, severity: "error", message: error.message },
       });
       console.log(error);
     }
